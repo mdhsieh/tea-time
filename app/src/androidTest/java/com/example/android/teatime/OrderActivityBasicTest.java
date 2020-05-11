ea2e17aaa9591dc20f519517b26e1ff676fab451
Michael Hsieh <mdhsieh8@gmail.com>
2020-05-11 11:47:40 -0700

    fix: Build successful after adding support-v4:28.0.0.
    Create OrderActivityBasicTest, which tests order increment.
    Test passes on API 28 emulator.

---
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -0,0 +1,32 @@
+package com.example.android.teatime;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class OrderActivityBasicTest {
+
+    @Rule
+    public ActivityTestRule<OrderActivity> activityTestRule =
+            new ActivityTestRule<>(OrderActivity.class);
+
+    @Test
+    public void clickIncrementButton_ChangesQuantityAndCost() {
+        //1. Find the view
+        //2. Perform action on the view
+        onView(withId(R.id.increment_button)).perform(click());
+        //3. Check if the view does what you expected
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("1")));
+        onView(withId(R.id.cost_text_view)).check((matches(withText("$5.00"))));
+    }
+}
