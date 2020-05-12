d3a3e1ea28e0e3244b89616c4a3c4d0b4ea945d8
Michael Hsieh <mdhsieh8@gmail.com>
2020-05-11 18:40:31 -0700

    feat: Finish TESP.01-Exercise-FinishOrderActivityBasicTest.
    Delete commented increment test. Decrement test verifies that
    the decrement button won't decrease the quantity 0 and cost below $0.00.

--- app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -13,30 +13,38 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+/**
+ * This test demos a user clicking the decrement button and verifying that it properly decrease
+ * the quantity the total cost.
+ */
+
 @RunWith(AndroidJUnit4.class)
 public class OrderActivityBasicTest {
 
+    /**
+     * The ActivityTestRule is a rule provided by Android used for functional testing of a single
+     * activity. The activity that will be tested will be launched before each test that's annotated
+     * with @Test and before methods annotated with @before. The activity will be terminated after
+     * the test and methods annotated with @After are complete. This rule allows you to directly
+     * access the activity during the test.
+     */
     @Rule
     public ActivityTestRule<OrderActivity> activityTestRule =
             new ActivityTestRule<>(OrderActivity.class);
 
-    /*@Test
-    public void clickIncrementButton_ChangesQuantityAndCost() {
-        //1. Find the view
-        //2. Perform action on the view
-        onView(withId(R.id.increment_button)).perform(click());
-        //3. Check if the view does what you expected
-        onView(withId(R.id.quantity_text_view)).check(matches(withText("1")));
-        onView(withId(R.id.cost_text_view)).check((matches(withText("$5.00"))));
-    }*/
 
     @Test
-    public void clickDecrementButton_NoChangesQuantityAndCost() {
-        //1. Find the view
-        //2. Perform action on the view
+    public void clickDecrementButton_ChangesQuantityAndCost() {
+        // Check the initial quantity variable is zero
+        onView((withId(R.id.quantity_text_view))).check(matches(withText("0")));
+
+        // Click on decrement button
         onView(withId(R.id.decrement_button)).perform(click());
-        //3. Check if the view does what you expected
+
+        // Verify that the decrement button won't decrease the quantity 0
         onView(withId(R.id.quantity_text_view)).check(matches(withText("0")));
+
+        // Verify that the increment button also doesn't decrease the cost below $0.00
         onView(withId(R.id.cost_text_view)).check((matches(withText("$0.00"))));
     }
 }
