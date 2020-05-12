28037cc5539bfdceac6cc82384aaa7a56b48b38b
Michael Hsieh <mdhsieh8@gmail.com>
2020-05-12 09:36:05 -0700

    feat: Finish TESP.02-Exercise-AddMenuActivityScreenTest.
    Demos a user clicking on a GridView item in MenuActivity,
    and opens up the corresponding OrderActivity.

--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -14,6 +14,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+// import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
@@ -27,19 +28,35 @@ import static org.hamcrest.Matchers.is;
  */
 public class MenuActivityScreenTest {
 
+    public static final String TEA_NAME = "Green Tea";
+
+    /**
+     * The ActivityTestRule is a rule provided by Android used for functional testing of a single
+     * activity. The activity that will be tested will be launched before each test that's annotated
+     * with @Test and before methods annotated with @Before. The activity will be terminated after
+     * the test and methods annotated with @After are complete. This rule allows you to directly
+     * access the activity during the test.
+     */
     @Rule
     public ActivityTestRule<MenuActivity> menuActivityActivityTestRule =
             new ActivityTestRule<>(MenuActivity.class);
 
+    /**
+     * Clicks on a GridView item and checks it opens up the OrderActivity with the correct details.
+     */
     @Test
     public void clickGridViewItem_OpensOrderActivity() {
 
-        // click the first item of the GridView, which is black tea
-        onData(is(instanceOf(Tea.class))).atPosition(0)
+        // Uses {@link Espresso#onData(org.hamcrest.Matcher)} to get a reference to a specific
+        // GridView item and clicks it.
+        // onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(1).perform(click());
+
+        // click the second item of the GridView, which is green tea
+        onData(is(instanceOf(Tea.class))).atPosition(1)
                 .perform(click());
 
         // check the order screen header has black tea
-        onView(withId(R.id.tea_name_text_view)).check(matches(withText("Black Tea")));
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText(TEA_NAME)));
     }
 
 }
